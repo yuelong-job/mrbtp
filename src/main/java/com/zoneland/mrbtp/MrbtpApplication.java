0749590ab193acb0c5ff9c84a34ca37dd820ccf9
luyl <dvjfuuwxmc19>
2021-07-05 21:24:21 +0800

    springboot初始化

---
+++ src/main/java/com/zoneland/mrbtp/MrbtpApplication.java
@@ -0,0 +1,13 @@
+package com.zoneland.mrbtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrbtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrbtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoneland/mrbtp/MrbtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoneland.mrbtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrbtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
